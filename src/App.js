8be12010e66518fe7a54a1c152e97eb29e112ec0
rubi-gallegos <rubi.g.llam@gmail.com>
2021-10-22 23:31:40 -0500

    using-hooks-01

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
-const Header = () => {
-  return (
-    <header>
-      <h1>In the kitchen with Rubi</h1>
-    </header>
-  );
-};
-
-const Main = (props) => {
-  return (
-    <section>
-      <p>Come to make interesting and delicious dishes with me!</p>
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish) => (
-          <li>{dish}</li>
-        ))}
-      </ul>
-    </section>
-  );
-};
-
-const Footer = (props) => {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-};
-
-const dishes = ["Japchae", "Kimchi Jeon", "Haemul Soondubu Jjigae", "Mandoo"];
-
 function App() {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
   return (
-    <div className="App">
-      <Header />
-      <Main dishes={dishes} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
+    <>
+      <h1>
+        Current emotion is {emotion} and {secondary}.
+      </h1>
+      <button onClick={() => setEmotion("happy")}>Make Happy</button>
+      <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
+      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
+      <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
+    </>
   );
 }
 
